6d3cc507e516629be25335c7dcd1f10099c80c11
Sam Baker <Sam.Baker101@gmail.com>
2022-05-30 11:14:15 -0400

    Added command line arg for iterations

--- speed_test.cpp
+++ speed_test.cpp
@@ -10,43 +10,43 @@
 
 #include "swap/swap.h"
 
+
 //defines
-#define ITERATIONS 10000000
-
-
 typedef std::chrono::microseconds microseconds_t; 
+
 
 //main
 int main(int argc, char * argv[]){
 	using std::cout;
 
+	int iterations = atoi(argv[1]);
 	int a = 5;
 	int b = 10;
 
 	auto start = std::chrono::high_resolution_clock::now();
 
-	for (int i = 0; i < ITERATIONS; i++){
+	for (int i = 0; i < iterations; i++){
 		//tempSwap(&a, &b); //seg fault in this one lol
 	}
 	
 	auto end = std::chrono::high_resolution_clock::now();
 
 	auto time_elapsed = std::chrono::duration_cast<microseconds_t>(end - start);
-	cout << "Time Elapsed For " << ITERATIONS << " Iterations of Fun1: " << time_elapsed.count() << "\n";
-	cout << "Avg MicroSecond Per Iteration: " << (time_elapsed.count()/double(ITERATIONS)) << "\n";
+	cout << "Time Elapsed For " << iterations << " Iterations of Fun1: " << time_elapsed.count() << "\n";
+	cout << "Avg MicroSecond Per Iteration: " << (time_elapsed.count()/double(iterations)) << "\n";
 
 
 	start = std::chrono::high_resolution_clock::now();
 
-	for (int i = 0; i < ITERATIONS; i++){
+	for (int i = 0; i < iterations; i++){
 		addSwap(&a, &b); 
 	}
 	
 	end = std::chrono::high_resolution_clock::now();
 
 	time_elapsed = std::chrono::duration_cast<microseconds_t>(end - start);
-	cout << "Time Elapsed For " << ITERATIONS << " Iterations of Fun2: " << time_elapsed.count() << "\n";
-	cout << "Avg MicroSecond Per Iteration: " << (time_elapsed.count()/double(ITERATIONS)) << "\n";
+	cout << "Time Elapsed For " << iterations << " Iterations of Fun2: " << time_elapsed.count() << "\n";
+	cout << "Avg MicroSecond Per Iteration: " << (time_elapsed.count()/double(iterations)) << "\n";
 
 
 }
